e94a026c5a49babfea4577e6bc8399977405c3e6
Mark Ulrich <71002222+BNS-MarkUlrich@users.noreply.github.com>
2023-05-17 22:34:18 +0200

    Refactored movement code

--- Assets/Scripts/Movement/Movement.cs
+++ Assets/Scripts/Movement/Movement.cs
@@ -3,40 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public abstract class Movement : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rigidbody;
-    [SerializeField] private Vector3 velocity;
-    [SerializeField] private float speed = 2f;
-    [SerializeField] private float turnTimer = 5f;
-    private float originalTurnTimer;
-    private bool shouldTurn;
-
-    private void Start()
-    {
-        originalTurnTimer = turnTimer;
-    }
-
-    private void Update()
-    {
-        turnTimer -= Time.deltaTime;
-        if (turnTimer <= 0)
-        {
-            shouldTurn = !shouldTurn;
-            MoveForward(shouldTurn);
-            turnTimer = originalTurnTimer;
-        }
-    }
-
-    private void MoveForward(bool turn)
-    {
-        if (turn)
-        {
-            rigidbody.velocity = -velocity * speed;
-        }
-        else
-        {
-            rigidbody.velocity = velocity * speed;
-        }
-    }
+    [SerializeField] protected Vector3 velocity;
+    [SerializeField] protected float speed = 2f;
+    [SerializeField] protected float turnTimer = 5f;
+    
+    protected Rigidbody Rigidbody;
 }
---
+++ Assets/Scripts/Movement/TempMovement.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempMovement : Movement
+{
+    private float originalTurnTimer;
+    private bool shouldTurn;
+
+    private void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+        originalTurnTimer = turnTimer;
+        
+        MoveForward(shouldTurn);
+    }
+
+    private void Update()
+    {
+        turnTimer -= Time.deltaTime;
+        if (turnTimer <= 0)
+        {
+            shouldTurn = !shouldTurn;
+            MoveForward(shouldTurn);
+            turnTimer = originalTurnTimer;
+        }
+    }
+
+    private void MoveForward(bool turn)
+    {
+        if (turn)
+        {
+            Rigidbody.velocity = -velocity * speed;
+        }
+        else
+        {
+            Rigidbody.velocity = velocity * speed;
+        }
+    }
+}
