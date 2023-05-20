03858361f74b61e831553b4ad45c753ffb7e46b0
Mark Ulrich <71002222+BNS-MarkUlrich@users.noreply.github.com>
2023-05-20 23:43:33 +0200

    Ships now full stop when reaching target

--- Assets/Scripts/Movement/ShipMovement.cs
+++ Assets/Scripts/Movement/ShipMovement.cs
@@ -7,12 +7,19 @@ public class ShipMovement : Movement
 {
     [SerializeField] protected Transform target;
 
+    protected float distanceToTarget;
+
     private void Update()
     {
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if (distanceToTarget < 2f)
+        {
+            MyRigidBody.velocity *= 0.5f * Time.deltaTime;
+            return;
+        }
+        
         MoveToTarget(target);
 
         RotateToTarget(target);
     }
-
-    
 }
