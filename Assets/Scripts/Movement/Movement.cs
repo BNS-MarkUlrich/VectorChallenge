c0a97919666522fb830c3ed9ea71ed49a3443284
Mark Ulrich <71002222+BNS-MarkUlrich@users.noreply.github.com>
2023-05-20 10:25:06 +0200

    Refactored some movement code

--- Assets/Scripts/Movement/Movement.cs
+++ Assets/Scripts/Movement/Movement.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public abstract class Movement : MonoBehaviour
 {
-    [SerializeField] protected float speed = 2f;
+    [SerializeField] protected float maxSpeed = 20f;
+    [SerializeField] protected float rotationSpeed = 5f;
     
     protected Rigidbody MyRigidBody;
     protected Vector3 Velocity => MyRigidBody.velocity;
@@ -14,5 +16,33 @@ public abstract class Movement : MonoBehaviour
     private void Awake()
     {
         MyRigidBody = GetComponent<Rigidbody>();
+    }
+    
+    protected void MoveToTarget(Transform target)
+    {
+        var velocityDirection = target.position - transform.position;
+
+        /*var velocityMagnitude = velocityDirection.magnitude;
+
+        var desiredVelocity = velocityDirection.normalized * velocityMagnitude;*/
+        
+        var angle = Vector3.Angle(velocityDirection, transform.forward) / 10;
+        
+        var currentSpeed = maxSpeed / angle;
+        
+        if (currentSpeed >= maxSpeed)
+        {
+            currentSpeed = maxSpeed;
+        }
+        
+        MyRigidBody.velocity = transform.forward.normalized * (currentSpeed / Mass);
+        //MyRigidBody.velocity = transform.forward.normalized * velocityMagnitude;
+    }
+    
+    protected Quaternion RotateToTarget(Transform rotationTarget)
+    {
+        var targetDirection = rotationTarget.position - transform.position;
+        var angle = Vector3.Angle(targetDirection, transform.forward) / 10;
+        return Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDirection), Time.deltaTime * (rotationSpeed / angle / Mass));
     }
 }
--- Assets/Scripts/Movement/ShipMovement.cs
+++ Assets/Scripts/Movement/ShipMovement.cs
@@ -5,34 +5,14 @@ using UnityEngine;
 
 public class ShipMovement : Movement
 {
-    [SerializeField] private bool startMoving;
-    
-    [SerializeField] private Transform target;
-    [SerializeField] private float rotationSpeed = 5f;
+    [SerializeField] protected Transform target;
 
     private void Update()
     {
-        if (!startMoving) return;
-        
-        var velocityDirection = target.position - transform.position;
+        MoveToTarget(target);
 
-        var velocityMagnitude = velocityDirection.magnitude;
-
-        var desiredVelocity = velocityDirection.normalized * velocityMagnitude;
-        
-        var angle = Vector3.Angle(velocityDirection, transform.forward);
-
-        var newAngle = angle / 10;
-        var newSpeed = speed / newAngle;
-        
-        if (newAngle < 1)
-        {
-            newSpeed = speed;
-        }
-        
-        MyRigidBody.velocity = transform.forward.normalized * (newSpeed / Mass);
-        //MyRigidBody.velocity = transform.forward.normalized * velocityMagnitude;
-
-        transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (velocityDirection), Time.deltaTime * (rotationSpeed / newAngle / Mass));
+        RotateToTarget(target);
     }
+
+    
 }
