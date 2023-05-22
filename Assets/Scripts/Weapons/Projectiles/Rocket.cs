7052db90c8bab8b0dca3257403588145d424cdb1
Mark Ulrich <71002222+BNS-MarkUlrich@users.noreply.github.com>
2023-05-22 21:38:20 +0200

    AreaOfEffect Detection works

--- Assets/Scripts/Weapons/Projectiles/Rocket.cs
+++ Assets/Scripts/Weapons/Projectiles/Rocket.cs
@@ -8,27 +8,42 @@ public class Rocket : Projectile
     [SerializeField] private float areaRadius = 5f;
     [SerializeField] private float lifeTimer = 8f;
     
-    private Collider[] targetsInRange;
+    [SerializeField] private Collider[] targetsInRange;
     private Vector3 followVelocity;
     
     protected float distanceToTarget;
+    
+    public override void InitBullet(Turret newOrigin)
+    {
+        base.InitBullet(newOrigin);
+        target = origin.Target;
+    }
 
     private void AreaOfEffect()
     {
-        Physics.OverlapSphereNonAlloc(transform.position, areaRadius, targetsInRange);
-        print(targetsInRange.Length);
+        targetsInRange = Physics.OverlapSphere(transform.position, areaRadius);
+
+        if (targetsInRange.Length == 1)
+            return;
+
+        for (int i = 0; i < targetsInRange.Length; i++)
+        {
+            if (targetsInRange[i].transform == transform)
+                continue;
+            
+            
+            print(targetsInRange[i].name);
+        }
     }
 
     private void FollowTarget()
     {
-        //var velocityDirection = target.position - transform.position;
-        
         distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
         lifeTimer -= Time.deltaTime;
-        if (lifeTimer <= 0 || distanceToTarget < 10f)
+        if (lifeTimer <= 0 || distanceToTarget < 1f)
         {
             AreaOfEffect();
-            Destroy(this);
+            Destroy(gameObject);
             return;
         }
         
@@ -39,12 +54,6 @@ public class Rocket : Projectile
     private void Update()
     {
         FollowTarget();
-    }
-
-    public override void InitBullet(Turret newOrigin)
-    {
-        base.InitBullet(newOrigin);
-        target = origin.Target;
     }
 
     private void OnDrawGizmos()
