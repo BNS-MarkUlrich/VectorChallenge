aa925a6dbd0a16b486c2fdc5d18f4d07f1c5598d
Mark Ulrich <71002222+BNS-MarkUlrich@users.noreply.github.com>
2023-07-18 12:30:11 +0200

    Fixed basic camera movement

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : Movement
@@ -7,21 +8,47 @@ public class PlayerMovement : Movement
     [SerializeField] private Camera _myCamera;
     private bool groundedPlayer;
 
+    private CharacterController characterController;
+    private Vector3 playerVelocity;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        groundedPlayer = characterController.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        playerVelocity.y += _gravityValue * Time.deltaTime;
+        characterController.Move(playerVelocity * Time.deltaTime);
+    }
+
     public void MovePlayer(Vector3 input)
     {
-        var velocity = input * maxSpeed;
+        var velocity = input * (Time.deltaTime * maxSpeed);
+        velocity.y = MyRigidBody.velocity.y;
 
         if (input != Vector3.zero)
         {
-            MyRigidBody.velocity = velocity;
+            //MyRigidBody.velocity = velocity;
+            characterController.Move(transform.TransformVector(velocity));
         }
     }
 
     public void RotatePlayer(Vector2 input)
     {
-        transform.Rotate(0, input.x, 0);
-        _myCamera.transform.Rotate(-input.y, input.x, 0);
-        var z = _myCamera.transform.eulerAngles.z;
-        _myCamera.transform.Rotate(0, 0, -z);
+        Transform myTransform;
+        (myTransform = transform).Rotate(0, input.x, 0);
+        var eulerAngles = myTransform.eulerAngles;
+        var z = eulerAngles.z;
+        var x = eulerAngles.x;
+        transform.Rotate(-x, 0, -z);
+        
+        _myCamera.transform.Rotate(-input.y,0,0);
     }
 }
